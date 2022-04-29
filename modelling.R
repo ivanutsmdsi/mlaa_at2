5f38f6e9234cb932c9ebc11b9a130f51a4331f65
Ivan <13975420@student.uts.edu.au>
2022-04-30 09:13:57 +1000

    added R script framework

---
+++ modelling.R
@@ -0,0 +1,42 @@
+###
+#
+# MLAA Assignment 2 Part A
+#
+# Students:
+# Ivan Cheung
+# Ryan
+# Dinh
+# John 
+#
+#
+#
+#
+###
+
+## Load Data
+
+
+
+## Data Cleaning
+
+
+## Model Analysis
+
+## Model 1
+
+## Model 2
+
+## Model 3
+
+## Model 4
+
+## Model Evaluations
+
+
+
+
+## Model Optimisations
+
+## 
+
+##
