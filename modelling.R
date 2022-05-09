cc5250561206427977e8f6779479cb99d76dcb5a
DinhTran82 <dinhtran82@gmail.com>
2022-05-09 16:48:38 +1000

    Added Random forest, gbm, train & test partition
    Rewrote the code using the caret package to split the data into
    train & test sets.

--- modelling.R
+++ modelling.R
@@ -34,7 +34,7 @@ df_raw <- df
 ## EDA
 ######### Ryan
 str(df)
-df$LIMIT_BAL <-  as.integer()
+df$LIMIT_BAL <-  as.integer(df$LIMIT_BAL)
 df$ID <- NULL # drop ID
 df %>% summarise_all(n_distinct) 
 # SEX has 6 unique values - should be only 2
@@ -52,6 +52,8 @@ df$default <- as.factor(df$default) # set as factor
 levels(df$default) # check factor, 1 = yes
 tb_default <- table(df$default) # check target distribution
 tb_default # 0:1 = 16974:6127
+
+
 # checking the ratio
 tbl_prop <- prop.table(tb_default)
 tbl_prop # we have unbalanced data. approx 0:1 = 3:1
@@ -148,7 +150,6 @@ table(df$NO_PAY_DELAY)
 # I believe we lose a lot of information here. This is like all or nothing approach. e.g. may miss one but make the rest on time. 
 
 ## Build Train and Test Set                                                                     ----
-set.seed(20220504)
 
 # Split data into testing and training with 75% for training on stratified method
 #trainIndex = createDataPartition(y = df$default, p = 0.75, list = F)
@@ -156,30 +157,45 @@ set.seed(20220504)
 #testing_df = df[-trainIndex, ]
 # easier this way?
 
-df_n <- subset(df, df$default == 'N')
-df_y <- subset(df, df$default == 'Y')
-train_n_size <- floor(0.80 * nrow(df_n))
-train_n_indices <- sample(seq_len(nrow(df_n)), size = train_n_size)
-train_y_size <- floor(0.80 * nrow(df_y))
-train_y_indices <- sample(seq_len(nrow(df_y)), size = train_y_size)
+# df_n <- subset(df, df$default == 'N')
+# df_y <- subset(df, df$default == 'Y')
+# train_n_size <- floor(0.80 * nrow(df_n))
+# train_n_indices <- sample(seq_len(nrow(df_n)), size = train_n_size)
+# train_y_size <- floor(0.80 * nrow(df_y))
+# train_y_indices <- sample(seq_len(nrow(df_y)), size = train_y_size)
+# 
+# 
+# train_n <- df_n[train_n_indices, ]
+# test_n <- df_n[-train_n_indices, ]
+# train_y <- df_y[train_y_indices, ]
+# test_y <- df_y[-train_y_indices, ]
+# 
+# trainset <- rbind(train_n,train_y)
+# testset <- rbind(test_n, test_y)
+# 
+# rm(train_n_indices, train_n_size, train_n, df_n, df_y, train_n, test_n, train_y, test_y, train_y_indices, train_y_size)
+# 
+# # Validation of train and test set
+# table(trainset$default)
+# table(testset$default)
+# nrow(trainset) + nrow(testset)
+# nrow(df)
 
 
-train_n <- df_n[train_n_indices, ]
-test_n <- df_n[-train_n_indices, ]
-train_y <- df_y[train_y_indices, ]
-test_y <- df_y[-train_y_indices, ]
-
-trainset <- rbind(train_n,train_y)
-testset <- rbind(test_n, test_y)
-
-rm(train_n_indices, train_n_size, train_n, df_n, df_y, train_n, test_n, train_y, test_y, train_y_indices, train_y_size)
+#Dinh: REWORTE THE CODE ABOVE TO SPLIT INTO TRAINSET AND TESTSET USING CARET PACKAGE
+set.seed(20220504)
+intrain<-createDataPartition(y=df$default,p=0.8,list=FALSE)
+trainset<-df[intrain,]
+testset<-df[-intrain,]
 
 # Validation of train and test set
-table(trainset$default)
-table(testset$default)
 nrow(trainset) + nrow(testset)
 nrow(df)
 
+tab_temp1<-table(trainset[,"default"])
+print(prop.table(tab_temp1))
+tab_temp2<-table(testset[,"default"])
+print(prop.table(tab_temp2))
 
 
 
@@ -202,9 +218,126 @@ colnames(training_dn)[24] <- "default"
 table(training_dn$default)
 
 
+
+#-------------------------------------------####---------------------------#
 ### Model Analysis
 
-## Model 1 Dinh
+## Model 1 Dinh  Random  Forest & gbm  
+## AUTO TUNE MODEL  ##
+set.seed(42)
+fitControl <- trainControl(method = 'cv', number = 10, 
+                           savePredictions = 'final', classProbs = TRUE, summaryFunction = twoClassSummary)
+
+trainDinh <-trainset
+testDinh<-testset
+
+#changing for the caret package
+
+trainDinh <-trainset
+testDinh<-testset
+
+levels(trainDinh$default) <- c("no", "yes")
+levels(testDinh$default) <- c("no", "yes")
+
+## Change variables PAY_ATMX into factors
+trainDinh[, c(18:23)] <- lapply(trainDinh[, c(18:23)], factor)
+testDinh[, c(18:23)] <- lapply(testDinh[, c(18:23)], factor)
+
+
+
+#model
+forest1 <- train(default~., data=trainDinh, method="rf", trControl=fitControl, Importance=TRUE, metric="ROC")
+print(forest1)
+plot(forest1)
+
+#confusion matrix for test set
+p1 <- predict(forest1, testDinh, type="raw")
+confusionMatrix(p1, testDinh$default, positive = "yes")
+
+plot(varImp(forest1), main="Important features plot") 
+
+#### Manual tunning
+tunegrid <- expand.grid(.mtry=13)
+modellist <- list()
+for (ntree in c(200, 250, 300, 350)) {
+  set.seed(42)
+  fit <- train(default~., data=trainDinh, method="rf",  tuneGrid=tunegrid, trControl=fitControl, ntree=ntree,Importance=TRUE,metric="ROC")
+  key <- toString(ntree)
+  modellist[[key]] <- fit
+}
+results <- resamples(modellist)
+summary(results)
+dotplot(results)
+
+#### Dinh tried gbm ######################
+### GBM Model #####
+set.seed(42)
+control<- trainControl(method = "cv",
+                       number = 10, #Making a simple cv for speed here
+                       search="grid",
+                       summaryFunction = twoClassSummary,
+                       classProbs = TRUE,
+                       allowParallel = TRUE
+)
+
+names(trainDinh)
+#####default grid
+gbm = train( x = trainDinh[, -24],  y = trainDinh$default,  
+             method = "gbm", 
+             trControl = control, verbose = FALSE, metric="ROC")
+
+
+gbm
+
+p1_gbm <-predict(gbm, testDinh)
+cfm_gbm <- confusionMatrix(data=p1_gbm, reference=as.factor(testDinh$default), positive="yes")
+cfm_gbm
+imp = varImp(gbm)
+# As a table
+imp
+# As a plot
+plot(imp, main="Important variables in the gbm model")
+
+
+#EXPAND GRID
+# #EXPAND GRID
+# gbm_grid =  expand.grid(
+#   interaction.depth = c(3,4),
+#   n.trees = (10:15)*100, 
+#   shrinkage = 0.01,
+#   n.minobsinnode = 10
+# )
+gbm_grid =  expand.grid(
+  interaction.depth = c(3,4,5),
+  n.trees = 2000, 
+  shrinkage = c(0.005,0.01),
+  n.minobsinnode = 10
+)
+set.seed(42)
+gbm_1 = train( x = trainDinh[, -24],  y = trainDinh$default, 
+               method = "gbm", 
+               trControl = control, verbose = FALSE,
+               tuneGrid= gbm_grid, metric="ROC")
+
+gbm_1
+
+
+p1_gbm <-predict(gbm_1, testDinh)
+cfm_gbm <- confusionMatrix(data=p1_gbm, reference=as.factor(tesDinh$default), positive="yes")
+cfm_gbm
+
+
+gbm_prob <- predict(gbm_1, testDinh, type = "prob")$"Y"
+perf_gbm <- prediction(gbm_prob, testDinh$default) %>% performance(measure = "tpr", x.measure = "fpr")
+plot(perf_gbm, col = "blue", lty = 2, main="ROC curve using gbm")
+abline(a=0,b=1)
+
+
+gbm_auc <-performance(prediction(gbm_prob, testDinh$default),"auc")
+gbm_auc<-unlist(slot(gbm_auc, "y.values"))
+gbm_auc
+
+#----------------------------------------------##---------------------------------------------------#
 
 ## Model 2 Ivan
 
